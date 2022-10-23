0d907c16fbec9d1972eb5d78cfe2e5010812d0a8
NikiDumb <tentotennot@mail.ru>
2022-10-23 23:44:14 +0300

    Revert "Revert "Reg for aspirants" (#2)"
    This reverts commit 1e69be3143504404cae54ddb41f23b1577be7c9a.

--- handlers/RegInsideHandler.py
+++ handlers/RegInsideHandler.py
@@ -5,7 +5,7 @@ from create_bot import dp, bot
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from keyboards.reply_keyboards import  UserMainMenu, RoleMenu
+from keyboards.reply_keyboards import  UserMainMenu, RoleMenu, AspirantMenu
 from sql_methods import sql_admins, sql_users
 from source import admin_states
 from source.admin_states import AdminState
@@ -16,11 +16,13 @@ slovar = {
 	'Соискатель' : 'aspirant',
 	'Разработчик' : 'razrab',
 	'Дизайнер' : 'designer',
-	'СММщик' : 'smm'
+	'СММщик' : 'smm',
+	'Организатор' : 'org'
 }
 
 class InsideSteps(StatesGroup):
 	Type = State()
+	Aspirant = State()
 	Add = State()
 
 async def WelcomeInsideProcess(message : types.Message, state : FSMContext):
@@ -33,15 +35,35 @@ async def TypeProcess(message : types.Message, state : FSMContext):
 	except:
 		await message.answer(f'{message.from_user.full_name}, выбери свою должность <u>на клавиатуре!</u>', reply_markup = RoleMenu)
 		return
+	if slovar[message.text] == 'aspirant':
+		await message.answer(f'{message.text} {message.from_user.full_name}, пожалуйста выбери свою будущую профессию', reply_markup = AspirantMenu)
+		await InsideSteps.next()
+	else:
+		await message.answer(f'Отлично, {message.text}, отправь мне своё <u>ФИО и фотографию</u> одним сообщением!', reply_markup = types.ReplyKeyboardRemove())
+		await InsideSteps.Add.set()
+
+async def AspirantProcess(message : types.Message, state : FSMContext):
+	try:
+		if slovar[message.text] == 'aspirant':
+			await message.answer(f'{message.from_user.full_name} <u>ТЫ УЖЕ СОИСКАТЕЛЬ</u>, попробуй еще раз', reply_markup = AspirantMenu)
+			return
+		await state.update_data(target = slovar[message.text])
+	except:
+		await message.answer(f'{message.from_user.full_name}, выбери свою должность <u>на клавиатуре!</u>', reply_markup = AspirantMenu)
+		return
 	await message.answer(f'Отлично, {message.text}, отправь мне своё <u>ФИО и фотографию</u> одним сообщением!', reply_markup = types.ReplyKeyboardRemove())
-	await InsideSteps.next()
+	await InsideSteps.Add.set()
+
 
 async def AddInsideProcess(message : types.Message, state : FSMContext):
 	if message.caption is None:
 		await message.answer(f'Попробуй еще раз, {message.from_user.full_name}')
 	else:
 		data = await state.get_data()
-		res = await sql_users.add_user(message.from_user.id, message.caption, data['type'], message.photo[0].file_id)
+		try:
+			res = await sql_users.add_user(message.from_user.id, message.caption, data['type'], data['target'], message.photo[0].file_id)
+		except:
+			res = await sql_users.add_user(message.from_user.id, message.caption, data['type'], 0, message.photo[0].file_id)
 		if res == 606:
 			await message.answer(f'{message.from_user.full_name} u already registred!', reply_markup = UserMainMenu)
 			await admin_states.SetUser()
@@ -53,4 +75,5 @@ async def AddInsideProcess(message : types.Message, state : FSMContext):
 def register_RegInsideHandlers(dp : Dispatcher):
 	dp.register_message_handler(WelcomeInsideProcess, commands = ['reg'], state = '*')
 	dp.register_message_handler(TypeProcess, state = InsideSteps.Type)
+	dp.register_message_handler(AspirantProcess, state= InsideSteps.Aspirant)
 	dp.register_message_handler(AddInsideProcess, content_types=['photo'],  state = InsideSteps.Add)
